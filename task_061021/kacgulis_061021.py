5a452e54c2822d5e76102a2f416d5ec0df1f14a3
Kristofers Solo <kristians.cagulis@gmail.com>
2021-10-08 18:14:01 +0300

    task 061021 update

--- task_061021/kacgulis_061021.py
+++ task_061021/kacgulis_061021.py
@@ -1,14 +1,14 @@
 # Author - Kristiāns Francis Cagulis
 # Date - 06.10.2021
 
-chapters = 61
+CHAPTERS = 61
 
 
 def read_array():
 	with open('book.txt', encoding='utf-8') as book:
 		lines = [line.strip('\n') for line in book]  # removes 'enter' characters
 		with open('array_output.txt', 'w') as output:
-			for i in range(1, chapters + 1):
+			for i in range(1, CHAPTERS + 1):
 				line = lines.index(f"Chapter {i}") + 1  # finds all chapter indexes/lines
 				output.write(f"Line {line} - Chapter {i}\n")  # writes line in file
 
@@ -18,7 +18,7 @@ def read_string():
 	with open('book.txt', encoding='utf-8') as book:
 		lines = book.read()
 		with open('str_output.txt', 'w') as output:
-			for i in range(1, chapters + 1):
+			for i in range(1, CHAPTERS + 1):
 				_, position = re.finditer(rf"\bChapter {i}\b", lines)  # finds all chapter positions
 				output.write(f"Position {position.start()} - Chapter {i}\n")  # writes position in file
 
