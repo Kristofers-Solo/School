ef13ea84985a6ea0b80ef91a5fdb151297e1f1ec
Kristofers Solo <kristians.cagulis@gmail.com>
2021-10-10 18:48:36 +0300

    added try|except

--- task_061021/kcagulis_061021.py
+++ task_061021/kcagulis_061021.py
@@ -1,9 +1,11 @@
 # Author - Kristiāns Francis Cagulis
 # Date - 06.10.2021
+import re
 
 CHAPTERS = 61
 
 
+# creates file with chapters and row numbers
 def read_array(document):
 	with open(document, encoding='utf-8') as book:
 		lines = [line.strip('\n') for line in book]  # removes 'enter' characters
@@ -13,8 +15,8 @@ def read_array(document):
 			output.write(f"Line {line} - Chapter {i}\n")  # writes line in file
 
 
+# creates file with chapter positions
 def read_string(document):
-	import re
 	with open(document, encoding='utf-8') as book:
 		lines = book.read()
 	with open('str_output.txt', 'w') as output:
@@ -23,10 +25,22 @@ def read_string(document):
 			output.write(f"Position {position.start()} - Chapter {i}\n")  # writes position in file
 
 
-def main():
-	document = input("Ievadiet faila nosaukumu: ")
+def read_book(document):
 	read_array(document)
 	read_string(document)
+
+
+def main():
+	try:
+		document = "book.txt"
+		read_book(document)
+	except:
+		try:
+			document = "1342-0.txt"
+			read_book(document)
+		except:
+			document = input("Ievadiet faila nosaukumu: ")
+			read_book(document)
 
 
 if __name__ == '__main__':
