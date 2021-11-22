dba6dee19e0dba601a5a86db257bc3be3752c0f1
Kristofers Solo <kristians.cagulis@gmail.com>
2021-11-22 17:35:39 +0200

    Updated .venv

--- task_171121/task_171121.py
+++ task_171121/task_171121.py
@@ -1,3 +1,16 @@
 # Author - Kristiāns Francis Cagulis
 # Date - 22.11.2021
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('company_sales_data.csv')
+
+
+x = range(len(df["month_number"]))
+plt.plot(x, df["total_profit"])
+plt.xticks(x, df["month_number"])
+plt.xlabel("Month number")
+plt.ylabel("Total profit")
+
+plt.show()
